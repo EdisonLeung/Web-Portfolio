78ca5caa46e50118f1a5f04aec9e017bc86b0660
Edison Leung <edleung@cs.washington.edu>
2023-01-02 16:22:51 -0800

    fix routing issue

--- src/App.js
+++ src/App.js
@@ -26,9 +26,9 @@ const App = () => {
 };
 const AppWrapper = () => {
   return (
-    <Router basename="/web-portfolio">
+    <HashRouter basename="/web-portfolio">
       <App />
-    </Router>
+    </HashRouter>
   );
 };
 
