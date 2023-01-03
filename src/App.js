f7aba596574adcd67af0843645c8b68fee03e6fe
Edison Leung <edleung@cs.washington.edu>
2023-01-02 17:01:49 -0800

    test aws console

--- src/App.js
+++ src/App.js
@@ -26,15 +26,15 @@ const App = () => {
 };
 const AppWrapper = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="/images/UW_Logo.png" className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
-      </header>
-    </div>
-    // <Router basename="/">
-      // <Home />
-    // </Router>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src="/images/UW_Logo.png" className="App-logo" alt="logo" />
+    //     <h1>Hello from V2</h1>
+    //   </header>
+    // </div>
+    <Router basename="/">
+      <Home />
+    </Router>
   );
 };
 
